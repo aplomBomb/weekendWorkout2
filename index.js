c46004343ab6f851d03ebd99cff1e4c416f2d5e4
aplombomb <seizethesteez@gmail.com>
2020-02-09 19:32:10 -0500

    performance metrics logged

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const { performance } = require("perf_hooks");
+
 const ogArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const adjArr = (arr, number) => {
+  const start = performance.now();
   //Convert the array to an object
   //And do the arithmetic to each item's
   //index value
@@ -20,14 +23,16 @@ const adjArr = (arr, number) => {
     };
   }, {});
 
+  const stop = performance.now();
+
+  console.log(`Execution time: ${(stop - start).toFixed(4)} milliseconds`);
+
   return Object.values(arrObj);
-
-
 };
 
-console.log('1: ', adjArr(ogArr, 1))
-console.log('-1: ', adjArr(ogArr, -1))
-console.log('2: ', adjArr(ogArr, 2))
-console.log('9: ', adjArr(ogArr, 9))
-console.log('420: ', adjArr(ogArr, 420));
-console.log('-69: ', adjArr(ogArr, -69));
+console.log("1: ", adjArr(ogArr, 1));
+console.log("-1: ", adjArr(ogArr, -1));
+console.log("2: ", adjArr(ogArr, 2));
+console.log("9: ", adjArr(ogArr, 9));
+console.log("420: ", adjArr(ogArr, 420));
+console.log("-69: ", adjArr(ogArr, -69));
